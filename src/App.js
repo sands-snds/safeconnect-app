baadb5481e2810873a1df3f35058bb0be7482703
Sandra <alessandra.nicole0603@gmail.com>
2026-07-18 12:44:03 +0800

    up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AdminPage from './pages/AdminPage';
 import ResidentPage from './pages/ResidentPage';
-import DonationPage from './pages/DonationPage';
-import VolunteerPage from './pages/VolunteerPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
@@ -18,8 +16,6 @@ function App() {
           <Route path="/admin" element={<AdminPage />} />
           
           <Route path="/resident" element={<ResidentPage />} />
-          <Route path="/donation" element={<DonationPage />} />
-          <Route path="/volunteer" element={<VolunteerPage />} />
         </Routes>
       </div>
     </Router>
